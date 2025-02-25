1d2618642cb0773d2071a10000c96b09fa2a200f
Catalin Casuneanu <catalin.casuneanu@jay-software.com>
2025-02-25 14:23:09 +0200

    feat: add player object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -7,13 +8,20 @@ def main():
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
